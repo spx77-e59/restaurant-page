96921832d810d9e7773b480a11d9e48af86d4a4d
somtsho <179671017+spx77-e59@users.noreply.github.com>
2024-09-30 15:27:03 +0600

    Add contact info to footer module

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # restaurant-page
+credit
+1. Photo by <a href="https://unsplash.com/@lauraperuchi?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Laura Peruchi</a> on <a href="https://unsplash.com/photos/a-dimly-lit-restaurant-with-a-checkered-floor-I9fsp4Wj2EQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,32 @@
 export default function () {
+  const footerDiv = document.createElement("div");
+  const contactInfoDiv = document.createElement("div");
+  const contactInfoHeading = document.createElement("h2");
   const copyRightText = document.createElement("p");
+
+  contactInfoHeading.textContent = "Contact Us";
+  contactInfoDiv.append(contactInfoHeading);
+
+  const contactInfo = [
+    {
+      name: "Address",
+      value: `123 Culinary Avenue,
+Foodie Town, FL 54321`,
+    },
+    { name: "Phone", value: `(123) 456-7890` },
+    { name: "email", value: `info@thegoldenspoon.com` },
+  ];
+
+  contactInfo.forEach((info) => {
+    const value = document.createElement("p");
+    value.innerText = info.value;
+
+    contactInfoDiv.append(value);
+  });
+
   copyRightText.textContent = "© 2024 The Golden Spoon. All Rights Reserved.";
-  return copyRightText;
+
+  footerDiv.append(contactInfoDiv, copyRightText);
+
+  return footerDiv;
 }
