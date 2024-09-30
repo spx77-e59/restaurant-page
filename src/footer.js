695611c18bb0e1d4769ebc9921b1f68a49965b4a
somtsho <179671017+spx77-e59@users.noreply.github.com>
2024-09-30 14:04:59 +0600

    Add footer text

---
+++ src/footer.js
@@ -0,0 +1,5 @@
+export default function () {
+  const copyRightText = document.createElement("p");
+  copyRightText.textContent = "© 2024 The Golden Spoon. All Rights Reserved.";
+  return copyRightText;
+}
